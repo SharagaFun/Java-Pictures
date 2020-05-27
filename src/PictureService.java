64ce7035eb56c88347980f1cee4ce7f8f43b964c
kolyanok <kolyan-okunok@ya.ru>
2020-05-27 15:43:28 +0300

    Refactor

---
+++ src/PictureService.java
@@ -0,0 +1,53 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class PictureService {
+    private static BufferedImage resize(BufferedImage img, int newW, int newH)
+    {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
+    }
+
+    public static double getSimilarityPercent (String path1, String path2) throws IOException
+    {
+        BufferedImage img1 = resize(ImageIO.read(new File(path1)), 300, 300);
+        BufferedImage img2 = resize(ImageIO.read(new File(path2)), 300, 300);
+
+        int width = img1.getWidth();
+        int height = img1.getHeight();
+        int width2 = img2.getWidth();
+        int height2 = img2.getHeight();
+        if (width != width2 || height != height2) {
+            throw new IllegalArgumentException(String.format("Images must have the same dimensions: (%d,%d) vs. (%d,%d)", width, height, width2, height2));
+        }
+
+        long diff = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                diff += pixelDiff(img1.getRGB(x, y), img2.getRGB(x, y));
+            }
+        }
+        long maxDiff = 3L * 255 * width * height / 100;
+
+        return 1.000 -   diff / maxDiff / 100.000;
+    }
+
+    private static int pixelDiff(int rgb1, int rgb2) {
+        int r1 = (rgb1 >> 16) & 0xff;
+        int g1 = (rgb1 >>  8) & 0xff;
+        int b1 =  rgb1        & 0xff;
+        int r2 = (rgb2 >> 16) & 0xff;
+        int g2 = (rgb2 >>  8) & 0xff;
+        int b2 =  rgb2        & 0xff;
+        return Math.abs(r1 - r2) + Math.abs(g1 - g2) + Math.abs(b1 - b2);
+    }
+}
